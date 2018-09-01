adabaf9691bcc0297e8fc1f18b6810650955bdf0
Javier Encina <36193725+encinafr@users.noreply.github.com>
2018-09-01 20:25:55 -0300

    Add files via upload

---
+++ Ejercicio11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int i;
+            int max = 0;
+            int min = 0;
+            int acum = 0;
+            double promedio;
+            
+                for ( i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Ingrese un numero");
+                    num = int.Parse(Console.ReadLine());
+                    while (!(Validacion.Validar(num, 100, 0)))
+                    {
+                        Console.WriteLine("Error,Fuera del rango");
+                        Console.WriteLine("Ingrese un numero");
+                        num = int.Parse(Console.ReadLine());
+                    }
+                    acum=acum +num;
+                    if (i == 0)
+                    {
+                        max = num;
+                        min = num;
+                    }
+                    if (num > max)
+                    {
+                        max = num;
+                    }
+                    if (num < min)
+                    {
+                        min = num;
+                    }
+                    Console.Clear();
+                }
+            promedio = (acum / (float)i);
+            Console.WriteLine("El max ingresado es: {0} el min {1} y el promedio {2}", max, min, promedio);
+                   
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ejercicio11/Validacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio11
+{
+    class Validacion
+    {
+        static public bool Validar(int valor, int max, int min)
+        {
+            bool retorno = false;
+            if (valor > max || valor < min)
+            {
+                return retorno;
+            }
+            else
+            {
+                retorno = true;
+
+            }
+            return retorno;
+        }
+
+    }
+}
