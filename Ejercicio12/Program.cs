a117051883e35e71d371bd2df5b2b48c2c05e6a6
Javier Encina <36193725+encinafr@users.noreply.github.com>
2018-09-01 21:57:43 -0300

    Add files via upload

---
+++ Ejercicio12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio12;
+
+namespace Ejercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma;
+            int num;
+            int num1;
+            char validar= 'N';
+            do
+            {
+                Console.WriteLine("Ingrese el primer valor:");
+                num = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el segundo valor:");
+                num1 = int.Parse(Console.ReadLine());
+
+                suma = num + num1;
+                Console.WriteLine("La suma es: {0}", suma);
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Presione S para seguir sumando");
+                validar =Convert.ToChar(Console.ReadLine());
+            } while (ValidarRespuesta.ValidaS_N(validar));
+
+        }
+    }
+}
