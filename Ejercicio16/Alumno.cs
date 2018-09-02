9be0ea706b6285b6ea56f8d5f352e8202b409bd9
Javier Encina <36193725+encinafr@users.noreply.github.com>
2018-09-01 23:59:05 -0300

    Add files via upload

---
+++ Ejercicio16/Alumno.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    class Alumno
+    {
+        byte _nota1;
+        byte _nota2;
+        float _notaFinal;
+        public string apellido;
+        public string nombre;
+        public int legajo;
+
+        public void CalcularFinal()
+        {
+            Random r = new Random();
+            if (this._nota1 >= 4 && this._nota2 >= 4)
+            {
+                this._notaFinal = r.Next(0, 10);//devuelve un numero entero aleatorio 
+            }
+            else
+            {
+                this._notaFinal = -1;
+            }
+        }
+
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            this._nota1 = notaUno;
+            this._nota2 = notaDos;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();// Te permite Cargar contenido para devolverlo
+
+            sb.AppendFormat("El alumno {1} {0} legajo {2} sacó {3} en el 1er examen y {4} en el 2do", this.apellido, this.nombre, this.legajo, this._nota1, this._nota2);
+           
+            if (this._notaFinal != -1)
+            {
+                sb.AppendFormat(" y su nota final fue {0}", this._notaFinal);
+              
+            }
+            else
+            {
+                sb.AppendFormat(" Alumno Desaprobado");
+                
+            }
+            sb.AppendFormat("\n");
+            return sb.ToString();
+        }
+    }
+
+
+
+    
+}
---
+++ Ejercicio16/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 16";
+            Alumno a1 = new Alumno();
+            a1.nombre = "Maluma";
+            a1.apellido = "Babe";
+            a1.legajo = 1234;
+
+            a1.Estudiar(2, 10);
+            a1.CalcularFinal();
+            Console.WriteLine(a1.Mostrar());
+
+            Alumno a2 = new Alumno();
+            a2.nombre = "Turrita";
+            a2.apellido = "...";
+            a2.legajo = 2345;
+            a2.Estudiar(5, 8);
+            a2.CalcularFinal();
+            Console.WriteLine(a2.Mostrar());
+
+            Alumno a3 = new Alumno();
+            a3.nombre = "Pepe";
+            a3.apellido = "Salta Salta";
+            a3.legajo = 2222;
+            a3.Estudiar(4, 4);
+            a3.CalcularFinal();
+            Console.WriteLine(a3.Mostrar());
+
+            Console.ReadKey();
+        }
+    }
+}
