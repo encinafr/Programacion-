27ba0b1e22c241fe5cd1071270e756f74563f5b2
Javier Encina <36193725+encinafr@users.noreply.github.com>
2018-09-06 11:31:48 -0300

    Add files via upload

---
+++ Ejercicio19/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio19;
+
+namespace Ejercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 19";
+
+            Sumador suma = new Sumador(1);
+
+            Sumador suma1 = new Sumador(4);
+
+            Sumador suma2 = new Sumador();
+
+            long a, b;
+            string c = "holA";
+            string d = "¿todo bien?";
+            b = 2;
+
+            a = suma + b;
+            if (!(suma1 | suma))
+            {
+                Console.WriteLine(suma.Sumar(a, b));
+                Console.WriteLine(suma.Sumar(c, d));
+                
+            }
+            Console.ReadKey();
+
+            
+            
+        }
+    }
+}
---
+++ Ejercicio19/Sumador.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio19
+{
+    class Sumador
+    {
+        private int _cantidadSumas;
+
+        public Sumador(int suma)
+        {
+            this._cantidadSumas = suma;
+        }
+
+        public Sumador()
+        {
+            Sumador suma = new Sumador(0);
+        }
+
+        public long Sumar(long a, long b)
+        {
+            return a + b;
+        }
+
+        public string Sumar(string a, string b)
+        {
+            return a +" "+ b;
+        }
+
+        public static implicit operator int(Sumador valor)
+        {
+
+            return valor._cantidadSumas;
+        }
+
+        public static long operator +(Sumador op1, Sumador op2)
+        {
+            long retorno = op1._cantidadSumas + op2._cantidadSumas;
+            return retorno;
+        }
+
+        public static bool operator |(Sumador op1, Sumador op2)
+        {
+            bool retorno = false;
+            if (op1._cantidadSumas == op2._cantidadSumas)
+            {
+                retorno = true;
+            }
+            
+            return retorno;
+        }
+
+    }
+}
