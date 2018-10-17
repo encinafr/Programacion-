1e935e4931fe74911310f4d4d056fdd6b7563ac4
Javier Encina <36193725+encinafr@users.noreply.github.com>
2018-10-17 11:21:23 -0300

    Ejercicio 42 De la guia 2017
    Ejercicio 42 De la guia 2017

---
+++ Solucion/Solucion/ClaseNuev.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucion
+{
+   public class Numero
+    {
+        public double _divided;
+
+        public Numero(int x, int y)
+        {
+            try
+            { _divided = Divide(x, y); }
+            catch (DivideByZeroException e)
+            { throw new UnaException(e.Message + "\nUna Excepcion.", e); }
+        }
+
+        public static double Divide(int x, int y)
+        {
+            double retorno = 0;
+
+            if (y != 0)
+            { retorno = x / y; }
+            else
+            { throw new DivideByZeroException("Intento Dividir por 0."); }
+
+            return retorno;
+        }
+    }
+
+    public class N2
+    {
+        public Numero _numero;
+
+        public N2(int x, int y)
+        {
+            try { this._numero = new Numero(x, y); }
+            catch (UnaException e)
+            { throw new MiExcepcion(e.Message + "\nMi excepcion", e); }
+        }
+    }
+
+    public class UnaException : Exception
+    {
+        public UnaException(string message, Exception inner) : base(message, inner) { }
+    }
+
+    public class MiExcepcion : Exception
+    {
+        public MiExcepcion(string message, Exception inner) : base(message, inner) { }
+    }
+
+}
+
---
+++ Solucion/Solucion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Solucion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                N2 num = new N2(2, 0);
+            }
+            catch (MiExcepcion e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
