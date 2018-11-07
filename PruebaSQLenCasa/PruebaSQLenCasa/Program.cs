f21dbdeb7c7fb9aa32ecc665ef06cd2fb195ae1f
Javier Encina <36193725+encinafr@users.noreply.github.com>
2018-11-06 21:02:10 -0300

    Terminado
    Agregue un metodo que borra
    un registro de la base de datos y otro
    que modifica.(Funciona todo)

---
+++ PruebaSQLenCasa/PruebaSQLenCasa/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Data;//Requerido para base de datos
+  
+
+namespace PruebaSQLenCasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Televisor> _lista = new List<Televisor>();
+
+            XmlSerializer serializador = new XmlSerializer(typeof(List<Televisor>));
+            XmlTextWriter text = new XmlTextWriter("Televisores.xml", Encoding.UTF8);
+            XmlTextReader lectorxml = new XmlTextReader("Televisores.xml");
+            serializador.Serialize(text, _lista);
+            text.Close();
+            SqlConnection conexion = new SqlConnection(Properties.Settings.Default.Conexion);// establacela conexion entre mi base de datos y mi aplicacion
+
+            SqlCommand comando = new SqlCommand();
+
+            comando.CommandText = "SELECT * FROM televisores";
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.Connection = conexion;
+            conexion.Open();
+            SqlDataReader lector = comando.ExecuteReader();// para leer de la base de datos
+            while (lector.Read())
+            {
+                Console.WriteLine(lector[0] + "-" + lector[1] + "-" + lector[2] + "-" + lector[3] + "-" + lector[4]);
+                _lista.Add(new Televisor(lector.GetInt32(0), lector.GetString(1), lector.GetDouble(2), lector.GetInt32(3), lector.GetString(4)));
+                //Console.WriteLine(lector["id"]);//tiene que coincidir con el nombre en la base de datos
+            }
+            List<Televisor> L = (List<Televisor>)serializador.Deserialize(lectorxml);
+              Console.ReadKey();
+
+            conexion.Close();
+
+            conexion.Open();
+            lector = comando.ExecuteReader();
+            DataTable Televisores = new DataTable("Televisores");
+            Televisores.Load(lector);
+
+            Televisores.WriteXmlSchema("Televisores_Esquema.xml");
+            Televisores.WriteXml("Televisroes_dt.xml");
+
+            DataTable Televisores2 = new DataTable();
+            Televisores2.ReadXmlSchema("Televisores_Esquema.xml");
+            Televisores2.ReadXml("Televisroes_dt.xml");
+
+            Televisor nuevaTele = new Televisor(6, "Sorny", 2222, 42, "Argentina");
+           // nuevaTele.Insertar();
+            //Televisor.Borrar(nuevaTele);
+            nuevaTele.Modificar();
+            
+        }
+    }
+}
---
+++ PruebaSQLenCasa/PruebaSQLenCasa/Televisor.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Data;//Requerido para base de datos
+
+namespace PruebaSQLenCasa
+{
+    public class Televisor
+    {
+        public int id;
+        public string marca;
+        public double precio;
+        public int pulgadas;
+        public string pais;
+
+        public Televisor()
+        {
+
+        }
+
+        public Televisor(int id, string marca, double precio, int pulg, string rancho)
+        {
+            this.id = id;
+            this.marca = marca;
+            this.precio = precio;
+            this.pulgadas = pulg;
+            this.pais = rancho;
+        }
+
+        public bool Insertar()
+        {
+            bool retValue = false;
+            SqlConnection conexion = new SqlConnection(Properties.Settings.Default.Conexion);
+            SqlCommand comando = new SqlCommand();
+
+            comando.CommandText = "Insert into Televisores values (" + this.id + ",'" + this.marca + "'," + this.precio + "," + this.pulgadas + ",'" + this.pais + "' )";
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+                conexion.Close();
+                retValue = true;
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+
+            return retValue;
+        }
+
+        public static Boolean Borrar(Televisor tele)
+        {
+            Boolean retorno = false;
+            SqlConnection conexion = new SqlConnection(Properties.Settings.Default.Conexion); //Se instancia el SqlConnection para poder utilizar la base
+
+            try
+            {
+                conexion.Open(); //Se abre la conexción con la base
+
+                SqlCommand comandosSQL = new SqlCommand("DELETE FROM Televisores WHERE [codigo] = " + tele.id, conexion); //Se instancia el objeto capaz de ejecutar comandos, pero se cambia la orden, siendo esta vez usada para borrar datos
+                /*DELETE borra todo el registro completo, sin importar el campo. De no tener con condicion WHERE, borra TODA LA BASE*/
+
+                int registrosAfectados = comandosSQL.ExecuteNonQuery();
+                //Console.WriteLine("RegistrosAfectados: {0}", registrosAfectados);
+
+                if (registrosAfectados > 0)
+                { retorno = true; }
+            }
+            catch (Exception e) { }
+            finally { conexion.Close(); }
+            return retorno;
+        }
+
+        public Boolean Modificar()
+        {
+            Boolean retorno = false;
+            SqlConnection conexion = new SqlConnection(Properties.Settings.Default.Conexion); //Se instancia el SqlConnection para poder utilizar la base
+
+            try
+            {
+                conexion.Open(); //Se abre la conexción con la base
+
+                SqlCommand comandosSQL = new SqlCommand("UPDATE Televisores SET [codigo] = " + this.id+ ", [marca] = '" + this.marca + "' , [precio] = " + this.precio + " WHERE [codigo] = 3", conexion); //Se instancia el objeto capaz de ejecutar comandos, pero se cambia la orden, siendo esta vez usada para actualizar datos
+                /*UPDATE cambia datos del registro, SET es quien hace los cambios. De nuevo, sin la condición, va a cambiar TODOS LOS REGISTROS DE LA BASE*/
+
+                int registrosAfectados = comandosSQL.ExecuteNonQuery();
+                //Console.WriteLine("RegistrosAfectados: {0}", registrosAfectados);
+
+                if (registrosAfectados > 0) { retorno = true; }
+            }
+            catch (Exception e) { }
+            finally { conexion.Close(); }
+            return retorno;
+        }
+    }
+}
